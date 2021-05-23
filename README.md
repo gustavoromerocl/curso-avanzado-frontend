ae789c131f9ba90fd288cc862177a87f1ca41092
romerodevelopments <gandresrp@gmail.com>
2021-05-23 00:14:09 -0400

    Webpack - HTML Webpack plugin

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 # npm install --save-dev webpack
 # npm install webpack-dev-server --save-dev
-# npm install webpack webpack-cli --save-dev
+# npm install webpack webpack-cli --save-
+# npm i --save-dev html-webpack-plugin
--- stories/webpack.config.js
+++ stories/webpack.config.js
@@ -1,13 +1,18 @@
-const { Module } = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-Module.exports = {
+const htmlWebpack = new HtmlWebpackPlugin({
+    template: './assets/index.template.html',
+    filename: 'index.html'
+})
+
+module.exports = {
     entry: './assets/javascript/entry.js',
     output: {
         publicPath: '/',
         path: path.join(__dirname, '..'),
         filename: 'dist/javascript/bundle.js'
     },
-    plugins: [],
+    plugins: [htmlWebpack],
     module: {}
 }
