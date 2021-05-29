9419a1836aa7c9e79f7e69bdbe47520309617332
romerodevelopments <gandresrp@gmail.com>
2021-05-29 14:08:31 -0400

    Google Maps - Colocar marcador en el mapa

--- stories/assets/maps.js
+++ stories/assets/maps.js
@@ -1,11 +1,20 @@
 function initMap(){
+
+    const coords = {
+        lat: -33.52098282505456, 
+        lng: -70.78443197382306
+    }
+
     let map = new google.maps.Map(document.getElementById('mapa'),{
-        center: {
-            lat: -33.52098282505456, 
-            lng: -70.78443197382306
-        },
+        center: coords,
         zoom: 16
     });
+
+    let marker = new google.maps.Marker({
+        position: coords,
+        map,
+        title: 'Home'
+    })
 }
 
 initMap();
