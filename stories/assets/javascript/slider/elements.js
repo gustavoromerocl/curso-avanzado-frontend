b5ab95151ddb9f5f71167acbe07658a6ae7a30bb
romerodevelopments <gandresrp@gmail.com>
2021-05-28 21:25:15 -0400

    Slider - Elementos del slider JSON

---
+++ stories/assets/javascript/slider/elements.js
@@ -0,0 +1,28 @@
+const elements = [
+    {
+        title: 'Lorem Ipsum',
+        subtitle: 'Ipsum',
+        image: '../../public/images/image2.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elitNam aliquid quod vel. Corporis eaque repellendus iusto modi aspernatur vero numquam cum, quis nulla maxime laborum corrupti fugiat, perspiciatis similique placeat.',
+    },
+    {
+        title: 'Lorem Ipsum 2',
+        subtitle: 'Ipsum 2',
+        image: '../../public/images/image4.jpg',
+        text: '2 Lorem ipsum dolor sit amet consectetur adipisicing elitNam aliquid quod vel. Corporis eaque repellendus iusto modi aspernatur vero numquam cum, quis nulla maxime laborum corrupti fugiat, perspiciatis similique placeat.',
+    }, 
+    {
+        title: 'Lorem Ipsum 3',
+        subtitle: 'Ipsum 3',
+        image: '../../public/images/image3.jpg',
+        text: '2 Lorem ipsum dolor sit amet consectetur adipisicing elitNam aliquid quod vel. Corporis eaque repellendus iusto modi aspernatur vero numquam cum, quis nulla maxime laborum corrupti fugiat, perspiciatis similique placeat.',
+    }, 
+    {
+        title: 'Lorem Ipsum 4',
+        subtitle: 'Ipsum 4',
+        image: '../../public/images/image1.jpg',
+        text: '2 Lorem ipsum dolor sit amet consectetur adipisicing elitNam aliquid quod vel. Corporis eaque repellendus iusto modi aspernatur vero numquam cum, quis nulla maxime laborum corrupti fugiat, perspiciatis similique placeat.',
+    }, 
+];
+
+export default elements;
