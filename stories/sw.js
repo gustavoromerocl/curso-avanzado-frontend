ce0f40f83f0be4da43b29fc501f6ef80d5d4aebc
romerodevelopments <gandresrp@gmail.com>
2021-05-30 17:24:20 -0400

    Service workers - Actualizar cache de archivos

--- stories/sw.js
+++ stories/sw.js
@@ -3,11 +3,23 @@
  * activate
  * fetch
  */
-const CACHE_NAME = 'STORIES_CACHE-v1';
+const CACHE_NAME = 'STORIES_CACHE-v2';
 
 self.addEventListener('install', function(){
     //Guardar archivos iniciales 
     caches.open(CACHE_NAME).then(function(cache){
-        cache.addAll(['/index.html'])
+        cache.addAll(['/index.html', '/dist/javascript/bundle.js'])
     })
-})
+});
+
+self.addEventListener('activate', function(ev){
+    ev.waitUntil( //indica que se debe cumplir primero la promesa antes de continuar
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if(CACHE_NAME !== cacheName) return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
