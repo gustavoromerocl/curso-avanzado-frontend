053d273a6a70d9b0b4011ddab068aaf50951a0f0
romerodevelopments <gandresrp@gmail.com>
2021-05-30 22:03:03 -0400

    Subir a produccion - Extraer estilos en un archivo css

--- stories/webpack/webpack.prod.js
+++ stories/webpack/webpack.prod.js
@@ -1,6 +1,12 @@
+//npm install extract-text-webpack-plugin
 const common = require('./webpack.common.js')
 const merge = require('webpack-merge');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSaas = ExtractTextPlugin({
+    filename: 'dist/css/[name].[contenthash].css'
+})
 module.exports = merge(common,{
     output: {
         publicPath: '.' //Convierte las rutas relativas a la carpeta que contiene el proyecto
@@ -9,8 +15,15 @@ module.exports = merge(common,{
         rules:[
             {
                 test: /\.scss$/,
-                use: ['style-loader','css-loader', 'sass-loader']
+                use: extractSaas.extract({
+                    use: [
+                        {loader: 'css-loader', options: {minimize: true}},
+                        {loader: 'sass-loader'}
+                    ]
+                })
             }
         ]
-    }
+    },
+    plugins: [extractSaas]
 })
+
